0fb3a397123b79b9db9449c7c9f04ba83dc9105f
Tim Malseed <t.malseed@gmail.com>
2018-12-21 22:15:30 +1100

    Added Facebook Stetho

--- app/src/main/java/com/simplecityapps/shuttle/ShuttleApp.kt
+++ app/src/main/java/com/simplecityapps/shuttle/ShuttleApp.kt
@@ -3,6 +3,7 @@ package com.simplecityapps.shuttle
 import android.app.Activity
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.facebook.stetho.Stetho
 import com.simplecityapps.shuttle.dagger.AppInjector
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -21,6 +22,8 @@ class ShuttleApp : Application(), HasActivityInjector {
         AppInjector.init(this)
 
         Timber.plant(Timber.DebugTree())
+
+        Stetho.initializeWithDefaults(this)
     }
 
     override fun activityInjector(): AndroidInjector<Activity> {
