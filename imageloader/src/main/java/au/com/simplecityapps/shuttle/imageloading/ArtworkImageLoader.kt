1f81d6cd5f459bd29a902bd2ec597b8b69d1abab
Tim Malseed <t.malseed@gmail.com>
2019-03-24 17:21:13 +1100

    ImageLoader enhancements..
    Added callback, to notify when image has loaded.
    
    Added option to set load priority

--- imageloader/src/main/java/au/com/simplecityapps/shuttle/imageloading/ArtworkImageLoader.kt
+++ imageloader/src/main/java/au/com/simplecityapps/shuttle/imageloading/ArtworkImageLoader.kt
@@ -1,9 +1,12 @@
 package au.com.simplecityapps.shuttle.imageloading
 
 import android.widget.ImageView
+import au.com.simplecityapps.shuttle.imageloading.glide.GlideImageLoader
 import com.simplecityapps.mediaprovider.model.Album
 import com.simplecityapps.mediaprovider.model.AlbumArtist
 import com.simplecityapps.mediaprovider.model.Song
+
+typealias CompletionHandler = ((GlideImageLoader.LoadResult) -> Unit)?
 
 interface ArtworkImageLoader {
 
@@ -14,13 +17,20 @@ interface ArtworkImageLoader {
          * @param radius corner radius, in DP
          */
         class RoundedCorners(val radius: Int) : Options()
+
+        class Priority(val priority: Priority) : Options() {
+
+            enum class Priority {
+                Low, Default, High, Max
+            }
+        }
     }
 
-    fun loadArtwork(imageView: ImageView, albumArtist: AlbumArtist, vararg  options: Options)
+    fun loadArtwork(imageView: ImageView, albumArtist: AlbumArtist, vararg options: Options, completionHandler: CompletionHandler)
 
-    fun loadArtwork(imageView: ImageView, album: Album, vararg  options: Options)
+    fun loadArtwork(imageView: ImageView, album: Album, vararg options: Options, completionHandler: CompletionHandler)
 
-    fun loadArtwork(imageView: ImageView, song: Song, vararg  options: Options)
+    fun loadArtwork(imageView: ImageView, song: Song, vararg options: Options, completionHandler: CompletionHandler)
 
     fun clear(imageView: ImageView)
 }
--- imageloader/src/main/java/au/com/simplecityapps/shuttle/imageloading/glide/GlideImageLoader.kt
+++ imageloader/src/main/java/au/com/simplecityapps/shuttle/imageloading/glide/GlideImageLoader.kt
@@ -1,31 +1,43 @@
 package au.com.simplecityapps.shuttle.imageloading.glide
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import au.com.simplecityapps.shuttle.imageloading.ArtworkImageLoader
+import au.com.simplecityapps.shuttle.imageloading.CompletionHandler
 import au.com.simplecityapps.shuttle.imageloading.glide.module.GlideApp
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.simplecityapps.mediaprovider.model.Album
 import com.simplecityapps.mediaprovider.model.AlbumArtist
 import com.simplecityapps.mediaprovider.model.Song
 
 class GlideImageLoader : ArtworkImageLoader {
 
-    override fun loadArtwork(imageView: ImageView, albumArtist: AlbumArtist, vararg options: ArtworkImageLoader.Options) {
-        loadArtwork(imageView, albumArtist as Any, *options)
+    sealed class LoadResult {
+        object Success : LoadResult()
+        object Failure : LoadResult()
     }
 
-    override fun loadArtwork(imageView: ImageView, album: Album, vararg options: ArtworkImageLoader.Options) {
-        loadArtwork(imageView, album as Any, *options)
+    override fun loadArtwork(imageView: ImageView, albumArtist: AlbumArtist, vararg options: ArtworkImageLoader.Options, completionHandler: CompletionHandler) {
+        loadArtwork(imageView, albumArtist as Any, *options, completionHandler = completionHandler)
     }
 
-    override fun loadArtwork(imageView: ImageView, song: Song, vararg options: ArtworkImageLoader.Options) {
-        loadArtwork(imageView, song as Any, *options)
+    override fun loadArtwork(imageView: ImageView, album: Album, vararg options: ArtworkImageLoader.Options, completionHandler: CompletionHandler) {
+        loadArtwork(imageView, album as Any, *options, completionHandler = completionHandler)
     }
 
-    private fun <T> loadArtwork(imageView: ImageView, `object`: T, vararg options: ArtworkImageLoader.Options) {
+    override fun loadArtwork(imageView: ImageView, song: Song, vararg options: ArtworkImageLoader.Options, completionHandler: CompletionHandler) {
+        loadArtwork(imageView, song as Any, *options, completionHandler = completionHandler)
+    }
+
+    private fun <T> loadArtwork(imageView: ImageView, `object`: T, vararg options: ArtworkImageLoader.Options, completionHandler: CompletionHandler) {
         val glideRequest = GlideApp
             .with(imageView.context)
             .load(`object`)
@@ -38,7 +50,29 @@ class GlideImageLoader : ArtworkImageLoader {
                 is ArtworkImageLoader.Options.RoundedCorners -> {
                     glideRequest.apply(RequestOptions.bitmapTransform(MultiTransformation(mutableListOf(CenterCrop(), RoundedCorners(option.radius)))))
                 }
+                is ArtworkImageLoader.Options.Priority -> {
+                    when (option.priority) {
+                        ArtworkImageLoader.Options.Priority.Priority.Low -> glideRequest.priority(Priority.LOW)
+                        ArtworkImageLoader.Options.Priority.Priority.Default -> glideRequest.priority(Priority.NORMAL)
+                        ArtworkImageLoader.Options.Priority.Priority.High -> glideRequest.priority(Priority.HIGH)
+                        ArtworkImageLoader.Options.Priority.Priority.Max -> glideRequest.priority(Priority.IMMEDIATE)
+                    }
+                }
             }
+        }
+
+        completionHandler?.let {
+            glideRequest.addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    completionHandler(LoadResult.Failure)
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    completionHandler(LoadResult.Success)
+                    return false
+                }
+            })
         }
 
         glideRequest.into(imageView)
