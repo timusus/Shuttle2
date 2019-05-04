f887139364fff8627d26d38b3aacd382b343171c
Tim Malseed <t.malseed@gmail.com>
2019-05-04 14:26:41 +1000

    Fixed an issue where SpacesItemDecoration used 0 padding for first item

--- app/src/main/java/com/simplecityapps/shuttle/ui/common/recyclerview/SpacesItemDecoration.kt
+++ app/src/main/java/com/simplecityapps/shuttle/ui/common/recyclerview/SpacesItemDecoration.kt
@@ -5,13 +5,13 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.simplecityapps.shuttle.ui.common.utils.dp
 
-class SpacesItemDecoration(space: Int) : RecyclerView.ItemDecoration() {
+class SpacesItemDecoration(space: Int, val skipFirst: Boolean = false) : RecyclerView.ItemDecoration() {
 
     private val space: Int = space.dp
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
 
-        if (parent.getChildLayoutPosition(view) == 0) {
+        if (skipFirst && parent.getChildLayoutPosition(view) == 0) {
             outRect.left = 0
             outRect.right = space
         } else {
