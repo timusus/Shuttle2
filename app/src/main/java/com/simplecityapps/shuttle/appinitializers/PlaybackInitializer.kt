3ec13cfc87fa46d6df5172ed34c3d99b01cc7010
Tim Malseed <t.malseed@gmail.com>
2019-11-07 18:07:23 +1100

    Remove unused code

--- app/src/main/java/com/simplecityapps/shuttle/appinitializers/PlaybackInitializer.kt
+++ app/src/main/java/com/simplecityapps/shuttle/appinitializers/PlaybackInitializer.kt
@@ -186,18 +186,3 @@ class PlaybackInitializer @Inject constructor(
         }
     }
 }
-
-
-inline fun <T : Any> guardLet(vararg elements: T?, closure: () -> Nothing): List<T> {
-    return if (elements.all { it != null }) {
-        elements.filterNotNull()
-    } else {
-        closure()
-    }
-}
-
-inline fun <T : Any> ifLet(vararg elements: T?, closure: (List<T>) -> Unit) {
-    if (elements.all { it != null }) {
-        closure(elements.filterNotNull())
-    }
-}
