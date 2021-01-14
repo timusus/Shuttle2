92459b5e048c86aa51a6d411b36faf1eaec0a02c
Tim Malseed <t.malseed@gmail.com>
2021-01-14 22:38:09 +1100

    Fixed an issue where Emby scan couldn't complete..
    .. because their documentation is wrong

--- mediaprovider/emby/src/main/java/com/simplecityapps/provider/emby/http/Item.kt
+++ mediaprovider/emby/src/main/java/com/simplecityapps/provider/emby/http/Item.kt
@@ -5,11 +5,11 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Item(
-    @Json(name = "Name") val name: String,
+    @Json(name = "Name") val name: String?,
     @Json(name = "Id") val id: String,
     @Json(name = "RunTimeTicks") val runTime: Long?,
-    @Json(name = "Album") val album: String,
-    @Json(name = "AlbumArtist") val albumArtist: String,
+    @Json(name = "Album") val album: String?,
+    @Json(name = "AlbumArtist") val albumArtist: String?,
     @Json(name = "IndexNumber") val indexNumber: Int?,
     @Json(name = "ProductionYear") val productionYear: Int?,
     @Json(name = "Genres") val genres: List<String> = emptyList(),
