915f228a26325d8dd40d6b7fbe1f2724ddb91717
Tim Malseed <t.malseed@gmail.com>
2021-02-11 13:40:43 +1100

    Removed Bugsnag crash reporting tree..
    We can infer what happened from the logs. It's expensive to leave a breadcrumb for every log. And, we're no logging the fragment lifecycle anyway, so we know which screen the user is on

--- app/src/main/java/com/simplecityapps/shuttle/appinitializers/CrashReportingInitializer.kt
+++ app/src/main/java/com/simplecityapps/shuttle/appinitializers/CrashReportingInitializer.kt
@@ -3,17 +3,14 @@ package com.simplecityapps.shuttle.appinitializers
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import com.bugsnag.android.BreadcrumbType
 import com.bugsnag.android.Bugsnag
-import com.simplecityapps.shuttle.BuildConfig
 import com.simplecityapps.shuttle.persistence.GeneralPreferenceManager
 import com.simplecityapps.shuttle.ui.common.ActivityLifecycleCallbacksAdapter
-import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -24,10 +21,6 @@ class CrashReportingInitializer @Inject constructor(
     override fun init(application: Application) {
         if (preferenceManager.crashReportingEnabled) {
             Bugsnag.start(application)
-
-            if (!BuildConfig.DEBUG) {
-                Timber.plant(CrashReportingTree())
-            }
 
             application.registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacksAdapter {
                 override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
@@ -49,32 +42,5 @@ class CrashReportingInitializer @Inject constructor(
 
     override fun priority(): Int {
         return 1
-    }
-}
-
-
-class CrashReportingTree : Timber.Tree() {
-
-    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
-        Bugsnag.leaveBreadcrumb("${priority.getPriorityString()}/$tag\n$message")
-
-        try {
-            throwable?.let {
-                Bugsnag.notify(throwable)
-            }
-        } catch (error: Exception) {
-            Log.e("TimberInit", "Failed to log to CrashReportingTree. \n[ tag: $tag\nmessage: $message. ]", error)
-        }
-    }
-
-    private fun Int.getPriorityString(): String {
-        return when (this) {
-            Log.VERBOSE -> "V"
-            Log.DEBUG -> "D"
-            Log.INFO -> "I"
-            Log.WARN -> "W"
-            Log.ERROR -> "E"
-            else -> ""
-        }
     }
 }
