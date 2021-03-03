41d80db53019ed743b5bd6c8afbeea19f629e194
Tim Malseed <t.malseed@gmail.com>
2021-03-03 18:50:16 +1100

    Updated exoplayer extension update script

--- update-exoplayer.sh
+++ update-exoplayer.sh
@@ -1,23 +1,25 @@
-# Fetch latest exoplayer
+set -e
+
 cd ./exoplayer
 EXOPLAYER_ROOT="$(pwd)"
-NDK_PATH="/Users/tim/Library/Android/sdk/ndk/21.1.6352462"
+NDK_PATH="$ANDROID_SDK_ROOT/ndk/21.4.7075529"
 
-# Fetch latest exoplayer
-git pull
-
+echo "Updating FLAC Extension"
 # Update FLAC
 FLAC_EXT_PATH="${EXOPLAYER_ROOT}/extensions/flac/src/main"
-cd "${FLAC_EXT_PATH}/jni" && \
-curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz | tar xJ && \
-mv flac-1.3.2 flac
-cd "${FLAC_EXT_PATH}"/jni && \
-${NDK_PATH}/ndk-build APP_ABI=all -j4
+cd "${FLAC_EXT_PATH}/jni" &&
+  curl https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz | tar xJ &&
+  rm -rf flac &&
+  mv flac-1.3.2 flac
+cd "${FLAC_EXT_PATH}"/jni &&
+  "${NDK_PATH}"/ndk-build APP_ABI=all -j4
 
-# Update Opus=
+echo "Updating Opus Extension"
+# Update Opus
 OPUS_EXT_PATH="${EXOPLAYER_ROOT}/extensions/opus/src/main"
-cd "${OPUS_EXT_PATH}/jni" && \
-git clone https://github.com/xiph/opus.git libopus
-cd ${OPUS_EXT_PATH}/jni && ./convert_android_asm.sh
-cd "${OPUS_EXT_PATH}"/jni && \
-${NDK_PATH}/ndk-build APP_ABI=all -j4
+cd "${OPUS_EXT_PATH}/jni" &&
+  rm -rf libopus &&
+  git clone https://github.com/xiph/opus.git libopus
+cd "${OPUS_EXT_PATH}"/jni && ./convert_android_asm.sh
+cd "${OPUS_EXT_PATH}"/jni &&
+  "${NDK_PATH}"/ndk-build APP_ABI=all -j4
