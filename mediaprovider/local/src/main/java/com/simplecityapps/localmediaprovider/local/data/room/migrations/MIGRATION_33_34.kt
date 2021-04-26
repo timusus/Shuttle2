c168c1dfdc529e158d73491b14157326a76842e7
Tim Malseed <t.malseed@gmail.com>
2021-04-26 12:19:30 +1000

    Another possible migration fix

--- mediaprovider/local/src/main/java/com/simplecityapps/localmediaprovider/local/data/room/migrations/MIGRATION_33_34.kt
+++ mediaprovider/local/src/main/java/com/simplecityapps/localmediaprovider/local/data/room/migrations/MIGRATION_33_34.kt
@@ -5,6 +5,9 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_33_34 = object : Migration(33, 34) {
     override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP VIEW `AlbumData`")
+        database.execSQL("DROP VIEW `AlbumArtistData`")
+
         database.execSQL("CREATE TABLE IF NOT EXISTS songs2 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, track INTEGER, disc INTEGER, duration INTEGER NOT NULL, year INTEGER, path TEXT NOT NULL, albumArtist TEXT, artists TEXT NOT NULL, album TEXT, size INTEGER NOT NULL, mimeType TEXT NOT NULL, genres TEXT NOT NULL, lastModified INTEGER NOT NULL, playbackPosition INTEGER NOT NULL, playCount INTEGER NOT NULL, lastPlayed INTEGER, lastCompleted INTEGER, blacklisted INTEGER NOT NULL, mediaStoreId INTEGER, mediaProvider TEXT NOT NULL, replayGainTrack REAL, replayGainAlbum REAL)")
         database.execSQL("DROP INDEX IF EXISTS index_songs_path")
         database.execSQL("DROP INDEX IF EXISTS index_songs2_path")
@@ -16,8 +19,5 @@ val MIGRATION_33_34 = object : Migration(33, 34) {
         )
         database.execSQL("DROP TABLE songs")
         database.execSQL("ALTER TABLE songs2 RENAME TO songs")
-
-        database.execSQL("DROP VIEW `AlbumData`")
-        database.execSQL("DROP VIEW `AlbumArtistData`")
     }
 }
