7d3c5c08849c63b765e1c05691182cd9e785e150
Tim Malseed <t.malseed@gmail.com>
2021-05-03 14:44:57 +1000

    Bump to 0.3.0

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -1,5 +1,5 @@
 object AppVersion {
     const val versionMajor = 0
-    const val versionMinor = 2
-    const val versionPatch = 18
+    const val versionMinor = 3
+    const val versionPatch = 0
 }
