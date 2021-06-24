2b190153ae339cf6475c794ca9a4db3355a44ac6
Tim Malseed <t.malseed@gmail.com>
2021-06-24 10:43:58 +1000

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
-        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.gms:google-services:4.3.8")
         classpath("com.bugsnag:bugsnag-android-gradle-plugin:5.7.6")
         classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:8.8.5")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
