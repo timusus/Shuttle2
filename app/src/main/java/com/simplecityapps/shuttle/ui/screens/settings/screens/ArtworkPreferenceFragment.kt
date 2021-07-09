fd76b0e602c186a777d7822a4b9fed825bed6254
Tim Malseed <t.malseed@gmail.com>
2021-07-10 01:29:04 +1000

    Refactored settings into sub-screens

---
+++ app/src/main/java/com/simplecityapps/shuttle/ui/screens/settings/screens/ArtworkPreferenceFragment.kt
@@ -0,0 +1,70 @@
+package com.simplecityapps.shuttle.ui.screens.settings.screens
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import au.com.simplecityapps.shuttle.imageloading.ArtworkDownloadService
+import au.com.simplecityapps.shuttle.imageloading.ArtworkImageLoader
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.simplecityapps.shuttle.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ArtworkPreferenceFragment : PreferenceFragmentCompat() {
+
+    @Inject
+    lateinit var imageLoader: ArtworkImageLoader
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences_artwork, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+
+        preferenceScreen.findPreference<Preference>("pref_clear_artwork")?.setOnPreferenceClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.settings_dialog_title_clear_artwork))
+                .setMessage(getString(R.string.settings_dialog_message_clear_artwork))
+                .setPositiveButton(getString(R.string.settings_dialog_button_clear_artwork)) { _, _ ->
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        context?.let { context ->
+                            imageLoader.clearCache(context)
+                        }
+                    }
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close), null)
+                .show()
+
+            true
+        }
+
+        preferenceScreen.findPreference<Preference>("pref_download_artwork")?.setOnPreferenceClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.settings_dialog_title_download_artwork))
+                .setMessage(getString(R.string.settings_dialog_message_download_artwork))
+                .setPositiveButton(getString(R.string.settings_dialog_button_download_artwork)) { _, _ ->
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        context?.let { context ->
+                            context.startService(Intent(context, ArtworkDownloadService::class.java))
+                        }
+                    }
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close), null)
+                .show()
+
+            true
+        }
+
+    }
+}
---
+++ app/src/main/java/com/simplecityapps/shuttle/ui/screens/settings/screens/SettingsFragment.kt
@@ -0,0 +1,66 @@
+package com.simplecityapps.shuttle.ui.screens.settings.screens
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.simplecityapps.shuttle.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@Suppress("NAME_SHADOWING")
+@AndroidEntryPoint
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+
+        preferenceScreen.findPreference<Preference>("pref_screen_display")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.displayPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_media")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.mediaPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_artwork")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.artworkPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_widget")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.widgetPreferenceManager)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_playlist")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.playlistPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_app_info")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.appInfoPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_privacy")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.privacyPreferenceFragment)
+            true
+        }
+        preferenceScreen.findPreference<Preference>("pref_screen_debug")?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.debugPreferenceFragment)
+            true
+        }
+    }
+
+
+    // Static
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+}
