163be845c518834738fc2d62c65229843342c804
Tim Malseed <t.malseed@gmail.com>
2021-07-09 15:15:55 +1000

    Bump to 1.0.0-RC2

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -2,5 +2,5 @@ object AppVersion {
     const val versionMajor = 1
     const val versionMinor = 0
     const val versionPatch = 0
-    const val versionSuffix = "-RC1"
+    const val versionSuffix = "-RC2"
 }
