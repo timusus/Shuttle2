4dfecdb55d0a535dca23e1d8900746ae4da34b56
Tim Malseed <t.malseed@gmail.com>
2021-09-14 19:18:15 +1000

    Bump to 1.0.2-alpha04

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -2,5 +2,5 @@ object AppVersion {
     const val versionMajor = 1
     const val versionMinor = 0
     const val versionPatch = 2
-    const val versionSuffix = "-alpha03"
+    const val versionSuffix = "-alpha04"
 }
