4e8215727d265dfaaa5d3974f5bd83afea2ddbb6
Tim Malseed <t.malseed@gmail.com>
2022-08-12 06:36:27 +1000

    Remove Jenkins scripts

--- .jenkins/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp "$GOOGLE_SERVICES" ./androidApp/main/app/google-services.json
-
-chmod +x ./gradlew
-
-./gradlew :androidApp:main:app:publishReleaseBundle --parallel -PkeyAlias=shuttle -PkeyPass="$KEYSTORE_PASS" -PstorePass="$KEYSTORE_PASS"
