6e54a646da2f0d7af69efa71f82e362b304ac02d
Tim Malseed <t.malseed@gmail.com>
2022-09-20 20:27:23 +1000

    Bump to 1.0.5-beta3

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -2,5 +2,5 @@ object AppVersion {
     const val versionMajor = 1
     const val versionMinor = 0
     const val versionPatch = 5
-    val versionSuffix: String? = "beta2"
+    val versionSuffix: String? = "beta3"
 }
