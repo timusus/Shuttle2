81a5bf3ddf8a3ccc474c285b72a65065768d6d92
Tim Malseed <t.malseed@gmail.com>
2025-02-23 10:51:23 +1100

    Fix deployment (#165)
    * Update download-artifact github action
    
    * Bump app version to 1.0.8-alpha

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -1,6 +1,6 @@
 object AppVersion {
     const val versionMajor = 1
     const val versionMinor = 0
-    const val versionPatch = 7
-    val versionSuffix: String? = null
+    const val versionPatch = 8
+    val versionSuffix: String? = "alpha"
 }
