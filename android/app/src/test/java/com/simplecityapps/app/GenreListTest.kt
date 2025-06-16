22291eaaac24bba071b71c2c312eeef9bc955a5b
Jack Brockley <jack@jackbrockley.co.uk>
2025-06-17 00:08:26 +0100

    Better isolate genre sort unit tests

--- android/app/src/test/java/com/simplecityapps/app/GenreListTest.kt
+++ android/app/src/test/java/com/simplecityapps/app/GenreListTest.kt
@@ -3,6 +3,7 @@ package com.simplecityapps.app
 import com.simplecityapps.mediaprovider.MediaImportObserver
 import com.simplecityapps.mediaprovider.SongImportState
 import com.simplecityapps.mediaprovider.repository.genres.GenreRepository
+import com.simplecityapps.mediaprovider.repository.genres.comparator
 import com.simplecityapps.mediaprovider.repository.songs.SongRepository
 import com.simplecityapps.playback.PlaybackManager
 import com.simplecityapps.playback.queue.QueueManager
@@ -16,6 +17,7 @@ import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -35,7 +37,6 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class GenreListTest {
     private val testDispatcher = StandardTestDispatcher()
-    private val mediaProviders = listOf(MediaProviderType.MediaStore)
 
     private val genreRepository = mockk<GenreRepository>()
     private val songRepository = mockk<SongRepository>()
@@ -48,9 +49,6 @@ class GenreListTest {
     @Before
     fun setup() {
         Dispatchers.setMain(testDispatcher)
-
-        every { sortPreferenceManager.sortOrderGenreList } returns GenreSortOrder.Default
-        every { sortPreferenceManager.sortOrderGenreList = any() } just Runs
 
         every { mediaImportObserver.songImportState } returns
             MutableStateFlow(SongImportState.ImportComplete(MediaProviderType.MediaStore, ""))
@@ -66,14 +64,10 @@ class GenreListTest {
 
     @Test
     fun sortsOnInitPerPreferences() = runTest {
-        // Arbitrary list
-        val sortedGenres = listOf(
-            Genre(name = "Classical", 3, 10, mediaProviders),
-            Genre(name = "Electronic", 2, 10, mediaProviders),
-            Genre(name = "Rock", 1, 10, mediaProviders)
-        )
+        val sortedGenres = generateGenres()
         // Mock the output of the repository, matching the mocked sortOrderGenreList value
         every { genreRepository.getGenres(match { it.sortOrder == GenreSortOrder.Default }) } returns flowOf(sortedGenres)
+        every { sortPreferenceManager.sortOrderGenreList } returns GenreSortOrder.Default
 
         val viewModel = GenreListViewModel(
             genreRepository,
@@ -85,7 +79,7 @@ class GenreListTest {
             mediaImportObserver
         )
 
-        // Wait until ready, then capture state
+        // Capture ready state
         val result = viewModel.viewState.first { it is GenreListViewModel.ViewState.Ready }
 
         assertTrue(result is GenreListViewModel.ViewState.Ready)
@@ -95,13 +89,11 @@ class GenreListTest {
     }
 
     @Test
-    fun sortsByName() = runTest {
-        val genres = listOf(
-            Genre(name = "Pop", 1, 10, mediaProviders),
-            Genre(name = "Ambient", 2, 10, mediaProviders),
-            Genre(name = "Metal", 3, 10, mediaProviders)
-        )
+    fun setsSortOrderIfReady() = runTest {
+        val genres = generateGenres()
         every { genreRepository.getGenres(any()) } returns flowOf(genres)
+        every { sortPreferenceManager.sortOrderGenreList } returns GenreSortOrder.Default
+        every { sortPreferenceManager.sortOrderGenreList = any() } just Runs
 
         val viewModel = GenreListViewModel(
             genreRepository,
@@ -123,24 +115,13 @@ class GenreListTest {
 
         assertTrue(result is GenreListViewModel.ViewState.Ready)
         result as GenreListViewModel.ViewState.Ready
-        assertEquals(
-            listOf(
-                Genre(name = "Ambient", 2, 10, mediaProviders),
-                Genre(name = "Metal", 3, 10, mediaProviders),
-                Genre(name = "Pop", 1, 10, mediaProviders)
-            ),
-            result.genres
-        )
+        assertEquals(genres.sortedWith(GenreSortOrder.Name.comparator), result.genres)
     }
 
     @Test
-    fun sortsBySongCount() = runTest {
-        val genres = listOf(
-            Genre(name = "Pop", 1, 10, mediaProviders),
-            Genre(name = "Ambient", 2, 10, mediaProviders),
-            Genre(name = "Metal", 3, 10, mediaProviders)
-        )
-        every { genreRepository.getGenres(any()) } returns flowOf(genres)
+    fun skipsRedundantSort() = runTest {
+        every { genreRepository.getGenres(any()) } returns flowOf(generateGenres())
+        every { sortPreferenceManager.sortOrderGenreList } returns GenreSortOrder.Default
 
         val viewModel = GenreListViewModel(
             genreRepository,
@@ -155,20 +136,17 @@ class GenreListTest {
         // Wait until ready
         viewModel.viewState.first { it is GenreListViewModel.ViewState.Ready }
 
-        viewModel.setSortOrder(GenreSortOrder.SongCount)
+        viewModel.setSortOrder(GenreSortOrder.Default)
 
-        // Capture next emitted state
-        val result = viewModel.viewState.drop(1).first()
+        verify(exactly = 0) { sortPreferenceManager.sortOrderGenreList = any() }
+    }
 
-        assertTrue(result is GenreListViewModel.ViewState.Ready)
-        result as GenreListViewModel.ViewState.Ready
-        assertEquals(
-            listOf(
-                Genre(name = "Metal", 3, 10, mediaProviders),
-                Genre(name = "Ambient", 2, 10, mediaProviders),
-                Genre(name = "Pop", 1, 10, mediaProviders)
-            ),
-            result.genres
+    private fun generateGenres(count: Int = 5): List<Genre> = List(count) { index ->
+        Genre(
+            name = "Genre $index",
+            songCount = (5..20).random(),
+            duration = (10..100).random(),
+            mediaProviders = listOf(MediaProviderType.MediaStore)
         )
     }
 }
