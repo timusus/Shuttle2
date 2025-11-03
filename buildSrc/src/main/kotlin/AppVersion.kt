a1cddb81e2448fcf4b15649b1399e15d59515677
Tim Malseed <t.malseed@gmail.com>
2025-11-03 19:44:17 +1100

    Bump to 1.0.8

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -2,5 +2,5 @@ object AppVersion {
     const val versionMajor = 1
     const val versionMinor = 0
     const val versionPatch = 8
-    val versionSuffix: String? = "alpha01"
+    val versionSuffix: String? = ""
 }
