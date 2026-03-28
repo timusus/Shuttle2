4fcc8c3eec1d8da54dc37ede8562b368dd2d81fe
Tim Malseed <t.malseed@gmail.com>
2026-03-29 10:53:16 +1100

    Extract ShuffleAlbums use case for AlbumArtistDetail

---
+++ android/app/src/main/java/com/simplecityapps/shuttle/ui/common/playback/ShuffleAlbums.kt
@@ -0,0 +1,41 @@
+package com.simplecityapps.shuttle.ui.common.playback
+
+import com.simplecityapps.playback.PlaybackOperations
+import com.simplecityapps.playback.queue.QueueOperations
+import com.simplecityapps.shuttle.model.Song
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlinx.coroutines.suspendCancellableCoroutine
+
+class ShuffleAlbums @Inject constructor(
+    private val queueManager: QueueOperations,
+    private val playbackManager: PlaybackOperations,
+) {
+    sealed interface Result {
+        data object Success : Result
+        data class Failure(val message: String?) : Result
+    }
+
+    suspend operator fun invoke(songs: List<Song>): Result {
+        val shuffled = songs
+            .groupBy { it.album }
+            .entries
+            .shuffled()
+            .flatMap { it.value }
+
+        if (!queueManager.setQueue(shuffled)) {
+            return Result.Failure(null)
+        }
+        return suspendCancellableCoroutine { cont ->
+            playbackManager.load { result ->
+                result.onSuccess {
+                    playbackManager.play()
+                    cont.resume(Result.Success)
+                }
+                result.onFailure { error ->
+                    cont.resume(Result.Failure(error.message))
+                }
+            }
+        }
+    }
+}
