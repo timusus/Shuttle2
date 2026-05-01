b1ae552e77bf55150d6489f2239122d78ff87ff5
Àlex Magaz Graça <alexandre.magaz@gmail.com>
2026-05-01 19:05:56 +0200

    Add scrollbar to Playlists tab

--- android/app/src/main/java/com/simplecityapps/shuttle/ui/screens/library/playlists/PlaylistList.kt
+++ android/app/src/main/java/com/simplecityapps/shuttle/ui/screens/library/playlists/PlaylistList.kt
@@ -1,5 +1,6 @@
 package com.simplecityapps.shuttle.ui.screens.library.playlists
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +19,10 @@ import com.simplecityapps.shuttle.R
 import com.simplecityapps.shuttle.model.Playlist
 import com.simplecityapps.shuttle.model.SmartPlaylist
 import com.simplecityapps.shuttle.ui.common.components.CircularLoadingState
+import com.simplecityapps.shuttle.ui.common.components.FastScroller
 import com.simplecityapps.shuttle.ui.common.components.HorizontalLoadingView
 import com.simplecityapps.shuttle.ui.common.components.LoadingStatusIndicator
+import com.simplecityapps.shuttle.ui.common.components.NoPopup
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
@@ -98,47 +102,58 @@ private fun PlaylistList(
     onRename: (Playlist) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
-        modifier = modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(vertical = 8.dp),
-    ) {
-        if (smartPlaylists.isNotEmpty()) {
-            item {
-                Text(
-                    text = stringResource(R.string.playlists_title_smart_playlists),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
-                )
+    val state = rememberLazyListState()
+
+    Box(modifier = modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(vertical = 8.dp),
+            state = state,
+        ) {
+            if (smartPlaylists.isNotEmpty()) {
+                item {
+                    Text(
+                        text = stringResource(R.string.playlists_title_smart_playlists),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+                    )
+                }
+                items(smartPlaylists) { smartPlaylist ->
+                    SmartPlaylistListItem(
+                        smartPlaylist = smartPlaylist,
+                        onSmartPlaylistClick = onSmartPlaylistClick,
+                    )
+                }
             }
-            items(smartPlaylists) { smartPlaylist ->
-                SmartPlaylistListItem(
-                    smartPlaylist = smartPlaylist,
-                    onSmartPlaylistClick = onSmartPlaylistClick,
-                )
+            if (playlists.isNotEmpty()) {
+                item {
+                    Text(
+                        text = stringResource(R.string.playlists_title_playlists),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+                    )
+                }
+                items(playlists) { playlist ->
+                    PlaylistListItem(
+                        playlist = playlist,
+                        onPlaylistClick = onPlaylistClick,
+                        onPlay = onPlay,
+                        onAddToQueue = onAddToQueue,
+                        onPlayNext = onPlayNext,
+                        onDelete = onDelete,
+                        onClear = onClear,
+                        onRename = onRename,
+                    )
+                }
             }
         }
-        if (playlists.isNotEmpty()) {
-            item {
-                Text(
-                    text = stringResource(R.string.playlists_title_playlists),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
-                )
-            }
-            items(playlists) { playlist ->
-                PlaylistListItem(
-                    playlist = playlist,
-                    onPlaylistClick = onPlaylistClick,
-                    onPlay = onPlay,
-                    onAddToQueue = onAddToQueue,
-                    onPlayNext = onPlayNext,
-                    onDelete = onDelete,
-                    onClear = onClear,
-                    onRename = onRename,
-                )
-            }
-        }
+        FastScroller(
+            modifier = Modifier.fillMaxSize().padding(vertical = 8.dp),
+            state = state,
+            popup = ::NoPopup,
+            getPopupText = { null },
+        )
     }
 }
